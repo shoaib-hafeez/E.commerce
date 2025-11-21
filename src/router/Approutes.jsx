aad7c5c842c7aeea32dd9d493c4f23cec3a9c32d
shoaib-hafeez <shoaibhafeez94@gmail.com>
2025-11-21 07:42:10 +0500

    Auth (Login/Signup) setup with Tailwind + SCSS

---
+++ src/router/Approutes.jsx
@@ -0,0 +1,16 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Login from "./pages/Login";
+import Signup from "./pages/Signup";
+
+const AppRouter = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
+
+export default AppRouter;
